83e121bb0fb1c82d93e621752782f5bcdbe610bb
Tony Johnson <tony@tosp.net.au>
2018-08-26 18:08:40 +1000

    Approve spender allowance

--- contracts/TnyjToken.sol
+++ contracts/TnyjToken.sol
@@ -13,7 +13,14 @@ contract TnyjToken {
     uint256 _value
   );
 
+  event Approval (
+    address indexed _owner,
+    address indexed _spender,
+    uint256 _value
+  );
+
   mapping(address => uint) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
 
   constructor(uint256 _initialSupply) public {
     balanceOf[msg.sender] = _initialSupply;
@@ -25,6 +32,12 @@ contract TnyjToken {
     balanceOf[msg.sender] -= _value;
     balanceOf[_to] += _value;
     emit Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  function approve(address _spender, uint256 _value) public returns (bool success) {
+    allowance[msg.sender][_spender] = _value;
+    emit Approval(msg.sender, _spender, _value);
     return true;
   }
 }
--- test/TnyjToken.js
+++ test/TnyjToken.js
@@ -42,17 +42,36 @@ contract('TnyjToken', accounts => {
       assert.equal(success, true, 'it returns true')
       return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] })
     }).then(receipt => {
-      assert.equal(receipt.logs.length, 1, 'triggers one event');
-      assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
-      assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from');
-      assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the tokens are transferred to');
-      assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
+      assert.equal(receipt.logs.length, 1, 'triggers one event')
+      assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event')
+      assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from')
+      assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the tokens are transferred to')
+      assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount')
       return tokenInstance.balanceOf(accounts[1])
     }).then(balance => {
       assert.equal(balance.toNumber(), 250000, 'adds amount to receiving account')
       return tokenInstance.balanceOf(accounts[0])
     }).then(balance => {
       assert.equal(balance.toNumber(), 750000, 'deducts amount from sending account')
+    })
+  })
+
+  it('approves tokens for delegated transfer', () => {
+    return TnyjToken.deployed().then(instance => {
+      tokenInstance = instance
+      return tokenInstance.approve.call(accounts[1], 100)
+    }).then(success => {
+      assert.equal(success, true, 'it returns true')
+      return tokenInstance.approve(accounts[1], 100, { from: accounts[0] })
+    }).then(receipt => {
+      assert.equal(receipt.logs.length, 1, 'triggers one event')
+      assert.equal(receipt.logs[0].event, 'Approval', 'should be the "Approval" event')
+      assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the account the tokens are authorized by')
+      assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs the account the tokens are authorized to')
+      assert.equal(receipt.logs[0].args._value, 100, 'logs the transfer amount')
+      return tokenInstance.allowance(accounts[0], accounts[1])
+    }).then(allowance => {
+      assert.equal(allowance, 100, 'stores the allowance for delegated transfer')
     })
   })
 })
